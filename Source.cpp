f4525f6e0551d53d05cca66d1f062e55a2c16d53
Sirinoot Ketkham(Fukfang) <69237528+sirinoot@users.noreply.github.com>
2020-08-18 14:41:16 +0700

    ddd

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,11 @@
-//789
+#include<stdio.h>
+int main()
+{
+	printf("nickname = Fang\n");
+	printf("age = 18\n");
+	printf("birthdate = 18\\04\\2002\n");
+	printf("province = Bangkok");
+	return 0;
+}
+
+
